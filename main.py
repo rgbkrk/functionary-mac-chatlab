3a40fa2962982316725404b5b941b7b0edd21cb0
Kyle Kelley <rgbkrk@gmail.com>
2024-01-16 14:43:02 -0800

    switch to async

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from functionary.prompt_template import get_prompt_template_from_tokenizer
 from transformers import AutoTokenizer
 from huggingface_hub import hf_hub_download
 
+import asyncio
+
 # Model repository on the Hugging Face model hub
 model_repo = "meetkai/functionary-7b-v2-GGUF"
 
@@ -14,9 +16,6 @@ local_file_path = hf_hub_download(repo_id=model_repo, filename=file_name)
 
 # You can download gguf files from https://huggingface.co/meetkai/functionary-7b-v2-GGUF/tree/main
 llm = Llama(model_path=local_file_path, n_ctx=4096, n_gpu_layers=-1)
-messages = [
-    {"role": "user", "content": "what's the weather like in Hanoi?"}
-]
 
 # Create tokenizer from HF. 
 # We found that the tokenizer from llama_cpp is not compatible with tokenizer from HF that we trained
@@ -26,47 +25,77 @@ tokenizer = AutoTokenizer.from_pretrained("meetkai/functionary-7b-v2", legacy=Tr
 # prompt_template will be used for creating the prompt
 prompt_template = get_prompt_template_from_tokenizer(tokenizer)
 
-# Before inference, we need to add an empty assistant (message without content or function_call)
-messages.append({"role": "assistant"})
+def run_inference(messages, tools):
+    # Create the prompt to use for inference
+    prompt_str = prompt_template.get_prompt_from_messages(messages + [{"role": "assistant"}], tools)
+    token_ids = tokenizer.encode(prompt_str)
+
+    gen_tokens = []
+    # Get list of stop_tokens 
+    stop_token_ids = [tokenizer.encode(token)[-1] for token in prompt_template.get_stop_tokens_for_generation()]
+    # print("stop_token_ids: ", stop_token_ids)
+
+    # We use function generate (instead of __call__) so we can pass in list of token_ids
+    for token_id in llm.generate(token_ids, temp=0):
+        if token_id in stop_token_ids:
+            break
+        gen_tokens.append(token_id)
+
+    llm_output = tokenizer.decode(gen_tokens)
+
+    # parse the message from llm_output
+    response = prompt_template.parse_assistant_response(llm_output)
+
+    return response
 
 
-tools = [ # For functionary-7b-v2 we use "tools"; for functionary-7b-v1.4 we use "functions" = [{"name": "get_current_weather", "description":..., "parameters": ....}]
-    {
-        "type": "function",
-        "function": {
-            "name": "get_current_weather",
-            "description": "Get the current weather",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "location": {
-                        "type": "string",
-                        "description": "The city and state, e.g., San Francisco, CA"
-                    }
-                },
-                "required": ["location"]
-            }
+async def main():
+    messages = [
+        {"role": "user", "content": "what's the weather like in Santa Cruz, CA?"}
+    ]
+
+    from chatlab import tool_result, FunctionRegistry
+    from pydantic import Field
+
+    def get_current_weather(location: str = Field(description="The city and state, e.g., San Francisco, CA")):
+        """Get the current weather"""
+
+        return {
+            "temperature": 75,
+            "units": "F",
+            "weather": "sunny",
         }
-    }
-]
 
-# Create the prompt to use for inference
-prompt_str = prompt_template.get_prompt_from_messages(messages, tools)
-token_ids = tokenizer.encode(prompt_str)
+    fr = FunctionRegistry()
+    fr.register(get_current_weather)
 
-gen_tokens = []
-# Get list of stop_tokens 
-stop_token_ids = [tokenizer.encode(token)[-1] for token in prompt_template.get_stop_tokens_for_generation()]
-print("stop_token_ids: ", stop_token_ids)
+    print(fr.tools)
 
-# We use function generate (instead of __call__) so we can pass in list of token_ids
-for token_id in llm.generate(token_ids, temp=0):
-    if token_id in stop_token_ids:
-        break
-    gen_tokens.append(token_id)
+    response = run_inference(messages, fr.tools)
+    messages.append(response)
 
-llm_output = tokenizer.decode(gen_tokens)
+    if response.get('content') is not None:
+        print("Assistant: ", response['content'])
 
-# parse the message from llm_output
-result = prompt_template.parse_assistant_response(llm_output)
-print(result)
+    if response.get('tool_calls') is not None:
+        for tool in response['tool_calls']:
+            print(tool)
+
+            requested_function = tool['function']
+            result = await fr.call(requested_function['name'], requested_function['arguments'])
+            print(f"  𝑓  {requested_function['name']}(...) -> ", str(result))
+
+            tool_call_response = tool_result(tool['id'], content=str(result))
+            # OpenAI does not require the name field, but it is required for functionary's tool_result
+            tool_call_response['name'] = requested_function['name'] 
+
+            messages.append(tool_call_response)
+        
+        # Run inference again after running tools
+        response = run_inference(messages, fr.tools)
+        print("Assistant: ", response['content'])
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
